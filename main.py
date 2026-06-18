755ba3baf27b6f508226cd273fd562e53b27a0d9
b6jrgv89ty-lab <b6jrgv89ty@privaterelay.appleid.com>
2026-06-18 12:14:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,23 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-def jarvis(text):
-    text = text.lower()
-
-    if "hola" in text:
-        return "Sistema activo."
-
-    if "hora" in text:
-        from datetime import datetime
-        return f"Hora actual: {datetime.now().strftime('%H:%M')}."
-
-    return "No entendí el comando."
-
-
 @app.get("/")
 def home():
     return {"status": "Jarvis online"}
-
-@app.get("/ask")
-def ask(text: str):
-    return {"response": jarvis()o}
